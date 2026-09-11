590282f8fc0beac729644372f80b3f8025ec8e58
rbpatil0316 <rbpatil0316@gmail.com>
2026-09-11 18:29:53 +0530

    Add package __init__.py file

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+PDF Converter Package
+Convert various file formats to PDF with configurable resolution and file size optimization.
+"""
+
+__version__ = '1.0.0'
+__author__ = 'rbpatil0316'
+
+from .core.converter import PDFConverter
+
+__all__ = ['PDFConverter']
